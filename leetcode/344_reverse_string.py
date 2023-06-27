ca9e1ae9478b3e92f568f6475e468acab1d21c7d
Minsoo <markkim1@naver.com>
2023-06-27 18:22:07 +0900

    SOLVE 344. reverse string

---
+++ leetcode/344_reverse_string.py
@@ -0,0 +1,15 @@
+# 344 Reverse String
+# from typing import List
+
+# My answer
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s) // 2):
+            s[i], s[len(s) - 1 - i] = s[len(s) - 1 -i], s[i]
+        
+sol = Solution()
+sol.reverseString(["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ","c","a","n","a","l",":"," ","P","a","n","a","m","a"])
+
