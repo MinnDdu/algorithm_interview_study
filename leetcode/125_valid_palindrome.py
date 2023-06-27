8753e953a514caedc6879b8b583f6bb30985b3d9
Minsoo <markkim1@naver.com>
2023-06-27 17:50:21 +0900

    STUDY - 125. Valid Palindrome

--- leetcode/125_valid_palindrome.py
+++ leetcode/125_valid_palindrome.py
@@ -18,4 +18,36 @@ sol = Solution()
 sol.isPalindrome("A man, a plan, a canal: Panama")
 
 # study book - 1. w/ list
+def isPalindrome(s: str) -> bool:
+    preprocess = []
+    for c in s:
+        if c.isalnum():
+            preprocess.append(c.lower())
+    
+    while len(preprocess) > 1:
+        # List.pop(0) -> O(n)
+        if preprocess.pop(0) != preprocess.pop():
+            return False
+    return True
+isPalindrome("A man, a plan, a canal: Panama")
 
+# study book - 2. w/ deque
+import collections
+def isPalindrome(s: str) -> bool:
+    deque = collections.deque()
+    for c in s:
+        if c.isalnum():
+            deque.append(c.lower())
+    while len(deque) > 1:
+        # Deque.popleft() -> O(1)
+        if deque.popleft() != deque.pop():
+            return False
+    return True
+
+# study book - 3. w/ regex and slicing
+# slicing은 굉장히 효율적임 - 내부적으론 C언어로 구현되어있기때문
+import re
+def isPalindrome(s: str) -> bool:
+    s = s.lower()
+    s = re.sub('[^0-9a-z]', '', s)
+    return s == s[::-1]
