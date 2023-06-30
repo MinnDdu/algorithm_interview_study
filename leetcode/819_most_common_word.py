6e72f58c6ff9e68755ac65410a296553a94c6fca
Minsoo <markkim1@naver.com>
2023-06-30 18:07:21 +0900

    STUDY Most Common Word

--- leetcode/819_most_common_word.py
+++ leetcode/819_most_common_word.py
@@ -22,8 +22,28 @@ class Solution:
         return result[0]
     
 sol = Solution()
-print(sol.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ['hit']))
-print(sol.mostCommonWord("a, a, a, a, b,b,b,c, c", ['a']))
+# print(sol.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ['hit']))
+# print(sol.mostCommonWord("a, a, a, a, b,b,b,c, c", ['a']))
+
+# Study book - 1. defaultdict & regex
+import collections
+
+def mostCommonWord(paragraph: str, banned: list[str]) -> str:
+    paragraph = re.sub(r"[^\w]", ' ', paragraph.lower())
+
+    words = [word for word in paragraph.split() if word not in banned]
+    print(words)
+    
+    counts = collections.defaultdict(int)
+    for word in words:
+        counts[word] += 1
+    
+    return max(counts, key=counts.get)
 
 
+# Study book - 2. Counter & most_common()
+def mostCommonWord(paragraph: str, banned: list[str]) -> str:
+    words = [word for word in re.sub(r"[^\w]", ' ', paragraph).lower().split() if word not in banned]
+    counter = collections.Counter(words)
 
+    return counter.most_common(1)[0][0]
