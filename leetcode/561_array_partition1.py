31a7d6e6acd4ca6afd63a6c5c34f336508ac8fcf
Minsoo <markkim1@naver.com>
2023-07-20 14:13:08 +0900

    STUDY 561 Array Partition 1

--- leetcode/561_array_partition1.py
+++ leetcode/561_array_partition1.py
@@ -13,3 +13,15 @@ class Solution:
 s = Solution()
 print(s.arrayPairSum([6,2,6,5,1,2]))
 
+# Study book - 1. Even index summation w/ enumerate
+def arrayPartitionSum(nums: list[int]) -> int:
+    nums.sort()
+    result = 0
+    for i, n in enumerate(nums):
+        if i % 2 == 0:
+            result += n
+    return result
+
+# Study book - 2. Pythonic way -> list slicing
+def arrayPartitionSum(nums: list[int]) -> int:
+    return sum(sorted(nums)[::2])
