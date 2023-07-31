f7223b250e33d7b13dd5147879076bf5cc515bdc
Minsoo <markkim1@naver.com>
2023-07-31 13:49:21 +0900

    STUDY 206 Reverse Linked List

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -25,6 +25,32 @@ class Solution():
             node = node.next
 
         return rev_head
+    
+    # Study book - 1. Fliping w/ recursive method
+    def reverseList(self, head: ListNode) -> ListNode:
+        def reverse(node: ListNode, prev: ListNode = None) -> ListNode:
+            # Base case
+            if node is None:
+                return prev
+            next = node.next
+            node.next = prev
+            return reverse(next, node)
+        return reverse(head)
+            
+    
+
+    # Study book - 2. Fliping w/ iterative method
+    def reverseList(self, head: ListNode) -> ListNode:
+        node = head
+        prev = None
+
+        while node:
+            next = node.next
+            node.next = prev
+            prev = node
+            node = next
+        return prev
+    
 
 l5 = ListNode(5, None)
 l4 = ListNode(4, l5)
@@ -33,4 +59,3 @@ l2 = ListNode(2, l3)
 l1 = ListNode(1, l2)
 sol = Solution()
 print(sol.reverseList(l1))
-
