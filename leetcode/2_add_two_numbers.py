adcf7b1cf21fbbc6820994cc3a9abb3ed57d2395
Minsoo <markkim1@naver.com>
2023-08-03 21:11:37 +0900

    STUDY 2 Add Two Numbers

--- leetcode/2_add_two_numbers.py
+++ leetcode/2_add_two_numbers.py
@@ -43,6 +43,65 @@ class Solution():
             node = ListNode(val, prev)
 
         return node
+    
+    # Study book - 1. Data Structure Conversion
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> []:
+        def toList(lst: ListNode) -> [int]:
+            result: [] = []
+            while lst:
+                result.append(lst.val)
+                lst = lst.next
+            return result
+
+        def reverseList(lst: ListNode) -> ListNode:
+            node: ListNode = lst
+            prev: ListNode = None
+
+            while node:
+                tmp = node.next
+                node.next = prev
+                prev = node
+                node = tmp
+            return prev
+
+        def toReversedLinkedList(result: str) -> ListNode:
+            prev: ListNode = None
+            for c in result:
+                node: ListNode = ListNode(c, prev)
+                prev = node
+            return prev
+        
+        a = toList(reverseList(l1))
+        b = toList(reverseList(l2))
+
+        result = int(''.join(str(c) for c in a)) + int(''.join(str(c) for c in b))
+        return toReversedLinkedList(str(result))
+
+
+
+
+    # Study book - 2. Circuit Theory - Full Adder (전가산기) - Quotient(몫)은 Carry Out(자리올림수)로, Remainder(나머지)는 결과로 택하는 전체적인 Full Adder의 컨셉 이용
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            
+            # carry, val = (sum + carry) // 10, (sum + carry) % 10
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        
+        return root.next
+
 
 l3 = ListNode(3, None)
 l2 = ListNode(2, l3)
